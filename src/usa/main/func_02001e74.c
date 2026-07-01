87d5fcbb73572cbe30b053f159257e306473d0c1
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-07-01 22:10:46 +0100

    Match MED retriage function 02001e94

---
+++ src/usa/main/func_02001e74.c
@@ -0,0 +1,17 @@
+typedef struct {
+    unsigned int f_0;
+    unsigned int f_4;
+} Dims02001e94;
+
+void func_02001e74(Dims02001e94 *p, unsigned short *dst, int val, int shift) {
+    unsigned int outer;
+    unsigned int j;
+
+    for (outer = 0; outer < p->f_4; outer++) {
+        for (j = 0; j < p->f_0; j++) {
+            *dst++ = (unsigned short)(val | (shift << 12));
+            val++;
+        }
+        dst += 0x20 - p->f_0;
+    }
+}
