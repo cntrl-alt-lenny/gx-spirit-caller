8e65c3eeec5f7370dd8b06b24370097e851bb1f8
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-07-02 00:26:55 +0100

    Match MED function 02014814

---
+++ src/jpn/main/func_020147e0.c
@@ -0,0 +1,62 @@
+extern char *GetSystemWork(void);
+extern int func_02019000(void);
+extern int func_020195b8(void);
+extern int func_02019630(void);
+extern int func_ov000_021ae3a0(void);
+
+struct Bit02014814 {
+    unsigned int b0 : 1;
+};
+
+void func_020147e0(int *o) {
+    char *sw = GetSystemWork();
+    int x;
+
+    if (((struct Bit02014814 *)(sw + 0x8d8))->b0 == 0) {
+        return;
+    }
+    if (*(int *)(sw + 0x920) != 1) {
+        return;
+    }
+
+    switch (*(int *)(sw + 0x924)) {
+    case 0:
+        break;
+    case 1:
+        *(int *)(sw + 0x924) = 4;
+    case 4:
+        if ((unsigned int)func_02019000() < (unsigned int)(*(int *)(sw + 0x8dc) + 1)) {
+            return;
+        }
+        if (func_02019630() != 0x6c) {
+            return;
+        }
+        o[2] = 0x00090100;
+        x = *(int *)(sw + 0x8e0);
+        x = (x & ~1) | 1;
+        *(int *)(sw + 0x8e0) = x;
+        break;
+    case 3:
+        if (func_ov000_021ae3a0() == 2) {
+            *(int *)(sw + 0x91c) = 0xa;
+            *(int *)(sw + 0x920) = 1;
+            *(int *)(sw + 0x924) = 1;
+            *(int *)(sw + 0x8e0) |= 4;
+            break;
+        }
+        if (((struct Bit02014814 *)(sw + 0x8e0))->b0 != 0) {
+            return;
+        }
+        if (func_020195b8() == 0) {
+            o[2] = 0x00090101;
+        } else {
+            o[2] = 0x00090102;
+        }
+        break;
+    case 2:
+        o[2] = 0x00090103;
+        break;
+    default:
+        break;
+    }
+}
