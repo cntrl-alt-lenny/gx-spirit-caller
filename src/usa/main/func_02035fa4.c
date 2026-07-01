96a96e25674d57cb443a448b240e4853d37a59f1
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-07-01 15:42:48 +0100

    Match MED function 02035ff4

---
+++ src/usa/main/func_02035fa4.c
@@ -0,0 +1,34 @@
+extern unsigned char data_0219b200[];
+extern void func_02037c04(int a, int b, int c);
+extern int func_0203ab4c(int a);
+
+struct CardDef_02035ff4 {
+    unsigned char b0;
+    unsigned char b1;
+    char _pad[0xc - 2];
+};
+
+struct Hdr_02035ff4 {
+    char _pad0[0x8];
+    short cnt;
+    char _pad1[0x34 - 0xa];
+    unsigned char *defs;
+};
+
+int func_02035fa4(unsigned char *obj)
+{
+    struct Hdr_02035ff4 *hd = (struct Hdr_02035ff4 *)data_0219b200;
+    int id;
+    int hi;
+
+    id = (unsigned short)*(int *)(obj + 0x34);
+    hi = ((struct CardDef_02035ff4 *)(hd->defs + id * 0xc))->b1 >> 4;
+    if (hi != 0) {
+        hd->cnt = hd->cnt - 1;
+        if (hd->cnt <= 0) {
+            hd->cnt = 0;
+            func_02037c04(0, 0x7f, 8);
+        }
+    }
+    return func_0203ab4c(*(int *)obj);
+}
