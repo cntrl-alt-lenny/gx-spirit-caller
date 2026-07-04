2fdb336b93e8de5fe1dee4b89cd8b59107670e5c
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-07-04 19:06:01 +0100

    match: 2 more EUR main functions (brief 528 wave) - card-ID membership switches

---
+++ src/main/func_0202ee40.c
@@ -0,0 +1,17 @@
+int func_0202ee40(int a0) {
+    switch (a0) {
+    case 0x166a:
+    case 0x1670:
+    case 0x1671:
+    case 0x1672:
+    case 0x1681:
+    case 0x16b3:
+    case 0x16b4:
+    case 0x16cf:
+    case 0x17ee:
+    case 0x1945:
+    case 0x1946:
+        return 1;
+    }
+    return 0;
+}
