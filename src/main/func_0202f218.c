d176ccb6f30b4cb255913c956bc6b873cfe5846d
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-07-04 19:09:00 +0100

    match: func_0202f218 EUR (brief 528 wave) - card-ID membership switch, zero-shot

---
+++ src/main/func_0202f218.c
@@ -0,0 +1,16 @@
+int func_0202f218(int a0) {
+    switch (a0) {
+    case 0xfe4:
+    case 0xfe5:
+    case 0x1128:
+    case 0x1246:
+    case 0x1477:
+    case 0x181f:
+    case 0x182a:
+    case 0x182b:
+    case 0x182c:
+    case 0x183f:
+        return 1;
+    }
+    return 0;
+}
