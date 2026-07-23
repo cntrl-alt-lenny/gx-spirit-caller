397a98862aa0287c6756903bc7a47bfe0bc90ca9
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-07-23 18:13:57 +0100

    C-match: func_ov002_0227aa50 (queue cm-ov002-unknown-2, bitfield lever)

---
+++ src/overlay002/func_ov002_0227aa50.c
@@ -0,0 +1,22 @@
+typedef struct {
+    unsigned int byte0 : 8;
+    unsigned int byte1 : 8;
+    unsigned int byte2 : 8;
+    unsigned int byte3 : 8;
+} Field18_aa50;
+
+typedef struct {
+    char pad[0x16];
+    unsigned short field16;
+    Field18_aa50 field18;
+} DataCd300_aa50;
+
+extern DataCd300_aa50 data_ov002_022cd300;
+
+void func_ov002_0227aa50(void) {
+    data_ov002_022cd300.field18.byte0 = 0;
+    data_ov002_022cd300.field18.byte1 = 0;
+    data_ov002_022cd300.field18.byte2 = 0;
+    data_ov002_022cd300.field18.byte3 = 0;
+    data_ov002_022cd300.field16 |= 0xc0;
+}
