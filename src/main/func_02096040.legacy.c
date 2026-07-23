2085f6f1be6ff08dc7b710ce6a269a4907461193
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-07-23 21:29:20 +0100

    C-match: func_02096040 (queue cm-regalloc-ship, .legacy.c + volatile lever)

---
+++ src/main/func_02096040.legacy.c
@@ -0,0 +1,12 @@
+typedef struct {
+    int field0;
+    int field4;
+} Obj02096040;
+
+void func_02096040(Obj02096040 *arg0) {
+    volatile Obj02096040 tmp;
+    tmp.field0 = 0;
+    tmp.field4 = 0;
+    arg0->field0 = 0;
+    arg0->field4 = 0;
+}
