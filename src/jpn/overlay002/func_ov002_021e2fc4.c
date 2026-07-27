d29f582f07d1feee369660e378076ef4064cc5c8
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-07-27 23:09:19 +0100

    batch_port: jpn +1 ported [auto, jpn sha1 OK]

---
+++ src/jpn/overlay002/func_ov002_021e2fc4.c
@@ -0,0 +1,23 @@
+extern int func_ov002_021b96f4(int sign, unsigned short lo);
+extern int func_ov002_021fd72c(int a, int b, int c);
+extern int func_ov002_021e2d90(int a, int b, int c, int d);
+
+int func_ov002_021e2fc4(int arg0, int arg1, int arg2) {
+    int result;
+
+    if ((arg1 >> 1) == 0) {
+        unsigned sign;
+        unsigned short lo;
+        int r;
+
+        sign = ((unsigned)arg0 & 0x80000000) >> 31;
+        lo = (unsigned short)arg0;
+        r = func_ov002_021b96f4(sign, lo);
+        arg1 = (unsigned short)r;
+    }
+    result = func_ov002_021fd72c(arg0, arg1, arg2);
+    if (result == 0)
+        return 0;
+    func_ov002_021e2d90(0, arg0, arg1, arg2);
+    return 1;
+}
