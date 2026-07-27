b85e1566e346aa58952557a0f94362c68d723cd3
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-07-27 23:22:30 +0100

    batch_port: jpn +1 ported [auto, jpn sha1 OK]

---
+++ src/jpn/overlay002/func_ov002_022031a0.c
@@ -0,0 +1,19 @@
+typedef unsigned short u16;
+
+struct Self0 {
+    u16 f0;
+    u16 bit0 : 1;
+    u16 _rest0 : 11;
+    u16 f2_1213 : 2;
+    u16 _rest1 : 2;
+    u16 f4lo6 : 6;
+    u16 b6_14 : 9;
+    u16 f4b15 : 1;
+};
+
+extern int func_ov002_021ff05c(int bit0, int a, int bits, int c, int d);
+
+int func_ov002_022031a0(struct Self0 *self) {
+    if (func_ov002_021ff05c(self->bit0, 1, self->b6_14, 0, 1) != 0) return 2;
+    return self->f2_1213 == 0;
+}
