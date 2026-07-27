5186af11f90f3a19a0af7df31093c2c26a02af3c
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-07-27 21:01:58 +0100

    batch_port: usa +1 ported [auto, usa sha1 OK]

---
+++ src/usa/overlay002/func_ov002_02234800.c
@@ -0,0 +1,27 @@
+typedef struct {
+    unsigned short bit0 : 1;
+    unsigned short unused : 15;
+} halfword_bit0_t;
+
+typedef struct {
+    char pad_cec[0xcec];
+    int field_cec;
+} data_022d016c_t;
+extern data_022d016c_t data_ov002_022d008c;
+
+extern void func_ov002_021e2a4c(void);
+extern void func_ov002_021e1304(int x, int one, int match);
+extern void func_ov002_021e2b6c(void);
+
+int func_ov002_02234800(void *a) {
+    halfword_bit0_t *h = (halfword_bit0_t *)((char *)a + 2);
+    int i;
+    func_ov002_021e2a4c();
+    for (i = 0; i < 2; i++) {
+        int x = data_ov002_022d008c.field_cec ^ i;
+        int match = (x == h->bit0) ? 0 : 1;
+        func_ov002_021e1304(x, 1, match);
+    }
+    func_ov002_021e2b6c();
+    return 0;
+}
