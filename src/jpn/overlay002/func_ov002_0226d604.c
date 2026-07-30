588a65c3c6a47e304c6a574ab4823fa4bdff670c
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-07-30 20:43:48 +0100

    batch_port: jpn +2 ported [auto, jpn sha1 OK]

---
+++ src/jpn/overlay002/func_ov002_0226d604.c
@@ -0,0 +1,25 @@
+extern int data_ov002_022cd220;
+extern int func_ov002_02270508(int player, int i, int c);
+
+int func_ov002_0226d604(int arg0, int arg1) {
+    int val = data_ov002_022cd220;
+    int player;
+    int i;
+
+    val = (val & ~1) | (arg0 & 1);
+    val &= 0xff8000ff;
+    val |= ((unsigned int)arg1 << 17) >> 9;
+    data_ov002_022cd220 = val;
+
+    *(unsigned short *)((char *)&data_ov002_022cd220 + 0x16) |= 2;
+    *(unsigned char *)((char *)&data_ov002_022cd220 + 8) = 0;
+
+    for (player = 0; player < 2; player++) {
+        for (i = 0; i <= 4; i++) {
+            if (func_ov002_02270508(player, i, 0) != 0) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
