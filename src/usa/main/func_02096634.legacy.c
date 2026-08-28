2a5f2ccb14970d4adf68fd003511180374d6e060
Lenny <145166175+cntrl-alt-lenny@users.noreply.github.com>
2026-08-28 11:20:01 +0100

    batch_port: usa +20 ported [auto, usa sha1 OK]

---
+++ src/usa/main/func_02096634.legacy.c
@@ -0,0 +1,21 @@
+extern int OS_DisableIrq(void);
+extern void OS_RestoreIrq(int state);
+extern void func_02091924(void *self);
+
+void func_02096634(void *self, int param2) {
+    int irqState = OS_DisableIrq();
+    void *f0 = *(void **)((char *)self + 0x0);
+    void *f4 = *(void **)((char *)self + 0x4);
+    if (f0 != 0) {
+        *(void **)((char *)f0 + 0x4) = f4;
+    }
+    if (f4 != 0) {
+        *(void **)((char *)f4 + 0x0) = f0;
+    }
+    *(void **)((char *)self + 0x0) = 0;
+    *(void **)((char *)self + 0x4) = *(void **)((char *)self + 0x0);
+    *(int *)((char *)self + 0xc) &= ~0x4f;
+    *(int *)((char *)self + 0x14) = param2;
+    func_02091924((char *)self + 0x18);
+    OS_RestoreIrq(irqState);
+}
